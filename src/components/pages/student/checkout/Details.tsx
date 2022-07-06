5bc46f884c6f7f35f60b5954bae8cc08c40008a7
Damisi Babalola <dami600bab@gmail.com>
2022-07-06 16:23:03 +0100

    workinng on checkout

--- src/components/pages/student/checkout/Details.tsx
+++ src/components/pages/student/checkout/Details.tsx
@@ -3,10 +3,29 @@ import React from "react"
 function Details (){
 
     return(
-        <div>
-            details 
-        </div>
+        <div className="space-y-5 w-full">
+            
+            <div className="grid grid-cols-2 gap-x-4">
 
+                {/* order */}
+<div className="bg-white">
+rgggg
+
+</div>
+{/* amount */}
+<div className="bg-red-500">
+    fff
+</div>
+{/* comment */}
+
+
+            </div>
+
+            <div className="bg-red-200">
+packaging
+
+            </div>
+        </div>
     )
 }
 
